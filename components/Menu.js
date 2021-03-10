72310b083d313ca5be69094ad74383cb22ca8ba9
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-03-10 18:30:27 -0500

    adds final solution for menu.js

--- components/Menu.js
+++ components/Menu.js
@@ -22,25 +22,45 @@ let menuItems = [
 */
 
 const menuMaker = function(array) {
+
+  // menu is built out with the above HTML
+  const menu = document.createElement('div');
+  const ul = document.createElement('ul');
+
+  menu.classList.add('menu');
+  
+
   // Step 2: Inside the function, iterate over the array creating a list item <li> element for each item in the array.
   array.forEach(function(li){
-    console.log(li); // this is working
-  });
-  // Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
-  const menu = document.querySelector('.menu-button');
-  console.log(menu);
+    // console.log(li); // this is working
+    // console.log(document.querySelector('.menu > ul'));
+    const listItem = document.createElement('li');
 
-  menu.addEventListener('click', function(event){
-    menu.classList.toggle('menu-open');
-    console.log(event.target);
+    // console.log(li);
+    listItem.textContent = li;
+    
+    ul.appendChild(listItem);
   });
+
+  menu.appendChild(ul);
+  // Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+  const menuButton = document.querySelector('.menu-button');
+  // console.log(menuButton);
+
+  // Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+  menuButton.addEventListener('click', function(){
+    menu.classList.toggle('menu--open');
+  });
+
+  return menu;
+
 };
 
-menuMaker(menuItems);
+const header = document.querySelector('.header');
+header.appendChild(menuMaker(menuItems));
 
 
 
-// Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
 
 // Step 5: Don't forget to return your div.menu.
 
